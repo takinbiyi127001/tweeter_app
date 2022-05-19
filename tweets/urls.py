6e808397ed9cdac631f6f439d6a00274db478f27
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-05-19 18:47:45 +1000

    Function-based View and Form

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 
 from .views import TweetCreatView, TweetListView
-
+from . import views
 
 urlpatterns = [
-    path('', TweetListView.as_view(), name='home'),
-    path('tweets/new/', TweetCreatView.as_view(), name='tweet_new')
+    path('tweets/new/', views.tweet_new, name='tweet_new'),
+    path('', views.tweet_list, name='home'),
+
+
+    # path('', TweetListView.as_view(), name='home'),
+    # path('tweets/new/', TweetCreatView.as_view(), name='tweet_new')
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,21 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse
 from .models import Tweet
+from .forms import TweetForm
 
 
 # Create your views here.
 
-class TweetListView(ListView):
-    model = Tweet
-    template_name = 'tweets/home.html'
+# Class-based View
+# class TweetListView(ListView):
+#     model = Tweet
+#     template_name = 'tweets/home.html'
+#
+#
+# class TweetCreatView(CreateView):
+#     model = Tweet
+#     template_name = 'tweets/tweet_new.html'
+#     fields = ['user', 'body']
+#
+#     def get_success_url(self):
+#         return reverse('home')
 
 
-class TweetCreatView(CreateView):
-    model = Tweet
-    template_name = 'tweets/tweet_new.html'
-    fields = ['user', 'body']
+# Function-based View
 
-    def get_success_url(self):
-        return reverse('home')
+def tweet_list(request):
+    context = {
+        'object_list': Tweet.objects.all()
+    }
+    return render(request, 'tweets/home.html', context)
+
+
+def tweet_new(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TweetForm
+
+    return render(request, 'tweets/tweet_new.html', {'form': form})
